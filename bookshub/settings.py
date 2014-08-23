fa29bb1758b51df65354166b802955aa58e89cb1
Jose Padilla <jpadilla1@users.noreply.github.com>
2014-08-23 09:47:49 -0400

    install and setup django-configurations & django-dotenv

--- bookshub/settings.py
+++ bookshub/settings.py
@@ -10,73 +10,109 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+from configurations import Configuration, values
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+class Common(Configuration):
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0d^vy4-k=ubs_+7*p5qx693@5(!9d35=xyukxnqdj=5%i7dked'
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    ENVIRONMENT = values.Value(environ_prefix=None, default='DEVELOPMENT')
 
-TEMPLATE_DEBUG = True
+    SECRET_KEY = values.SecretValue(environ_prefix=None)
 
-ALLOWED_HOSTS = []
+    DEBUG = values.BooleanValue(False)
+
+    TEMPLATE_DEBUG = values.BooleanValue(DEBUG)
+
+    ALLOWED_HOSTS = ['*']
+
+    # Application definition
+
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+    )
+
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    ROOT_URLCONF = 'bookshub.urls'
+
+    WSGI_APPLICATION = 'bookshub.wsgi.application'
+
+    # Database
+    # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+    DATABASES = values.DatabaseURLValue(
+        'sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
+
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.6/topics/i18n/
+    LANGUAGE_CODE = 'en-us'
+
+    TIME_ZONE = 'UTC'
+
+    USE_I18N = True
+
+    USE_L10N = True
+
+    USE_TZ = True
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+    STATIC_ROOT = 'staticfiles'
+    STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+
+    TEMPLATE_DIRS = (
+        os.path.join(BASE_DIR, 'templates'),
+    )
+
+    SITE_ID = 1
+
+    DEFAULT_FROM_EMAIL = values.Value()
+    EMAIL_HOST = values.Value()
+    EMAIL_HOST_USER = values.Value()
+    EMAIL_HOST_PASSWORD = values.Value()
+    EMAIL_PORT = values.IntegerValue()
+    EMAIL_USE_TLS = values.BooleanValue(False)
 
 
-# Application definition
+class Development(Common):
+    DEBUG = True
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-)
+    TEMPLATE_DEBUG = DEBUG
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+    PROTOCOL = 'http'
 
-ROOT_URLCONF = 'bookshub.urls'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-WSGI_APPLICATION = 'bookshub.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # Dummy cache for development
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
     }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+class Production(Common):
+    DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
-STATIC_URL = '/static/'
+    # django-secure settings
+    PROTOCOL = 'https'
