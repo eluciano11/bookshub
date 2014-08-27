52588dc23e46f887e5d08e5a502be8bffdd97293
Jose Padilla <jpadilla1@users.noreply.github.com>
2014-08-27 12:28:27 -0400

    created models

---
+++ bookshub/accounts/constants.py
@@ -0,0 +1,10 @@
+ACCOUNT_TYPE_CHOICES = (
+    ('educational', 'educational'),
+    ('company', 'company'),
+    ('standard', 'standard')
+)
+
+ACCOUNT_STATUS_CHOICES = (
+    ('normal', 'normal'),
+    ('banned', 'banned')
+)
