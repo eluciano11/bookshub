31903e2a788ac8f616759ae3842c28e294974656
Jose Padilla <jpadilla1@users.noreply.github.com>
2014-09-09 22:37:35 -0400

    commented out unused static directory

--- bookshub/settings.py
+++ bookshub/settings.py
@@ -108,9 +108,9 @@ class Common(Configuration):
     STATIC_ROOT = 'staticfiles'
     STATIC_URL = '/static/'
 
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+    # STATICFILES_DIRS = (
+    #     os.path.join(BASE_DIR, 'static'),
+    # )
 
     TEMPLATE_DIRS = (
         os.path.join(BASE_DIR, 'templates'),
