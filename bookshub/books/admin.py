ae19fe242185c0923c56ae3b1c7a6d4c2484eefc
Emmanuel Luciano <emma.luciano11@gmail.com>
2014-09-13 13:59:13 -0400

    added list display to admin in books

--- bookshub/books/admin.py
+++ bookshub/books/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Book
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'price', 'start_date', 'quantity')
+
+admin.site.register(Book, BookAdmin)
