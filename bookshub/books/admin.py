12a79fcbf27240c82e18c769ca1e7238ed83980a
Emmanuel Luciano <emma.luciano11@gmail.com>
2014-09-14 00:32:50 -0400

    added review, image and category tables

--- bookshub/books/admin.py
+++ bookshub/books/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Review, Image, Category
 
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'price', 'start_date', 'quantity')
 
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'review', 'pub_date')
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'image')
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Category)
--- bookshub/books/constants.py
+++ bookshub/books/constants.py
@@ -2,3 +2,25 @@ BOOK_CONDITION = (
     ('new', 'New'),
     ('used', 'Used')
 )
+CATEGORY_CHOICES = (
+    ('accounting', 'Accounting'),
+    ('architecture', 'Architecture'),
+    ('art', 'Art'),
+    ('biology', 'Biology'),
+    ('business_administration', 'Business Administration'),
+    ('computer_science', 'Computer Science'),
+    ('design', 'Design'),
+    ('engineering', 'Engineering'),
+    ('english', 'English'),
+    ('french', 'French'),
+    ('finance', 'Finance'),
+    ('history', 'History'),
+    ('marketing', 'Marketing'),
+    ('mathematics', 'Mathematics'),
+    ('medical', 'Medical'),
+    ('philosophy', 'Philosophy'),
+    ('psychology', 'Psychology'),
+    ('spanish', 'Spanish'),
+    ('social_science', 'Social Science'),
+    ('travel', 'Travel')
+)
