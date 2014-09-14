ac85c98a093f1b7719ec3dcad74873ffada19cca
Emmanuel Luciano <emma.luciano11@gmail.com>
2014-09-14 00:32:50 -0400

    added review, image and category tables

--- bookshub/books/models.py
+++ bookshub/books/models.py
@@ -1,14 +1,21 @@
-from django.db import models
+import time
 
-from .constants import BOOK_CONDITION
+from django.db import models
+from .constants import BOOK_CONDITION, CATEGORY_CHOICES
 
 from taggit.managers import TaggableManager
+
+
+def get_book_image_path(filename):
+    return "books/%s_%s" % \
+           (str(time()).replace('.', '_'), filename)
 
 
 class Book(models.Model):
     description_help_text = """Tell us more about the condition of your book."""
 
     user = models.ForeignKey('users.User')
+    category = models.ForeignKey('Category')
 
     isbn_10 = models.CharField(max_length=10, blank=True)
     isbn_13 = models.CharField(max_length=13, blank=True)
@@ -31,3 +38,34 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    user = models.ForeignKey('users.User')
+    book = models.ForeignKey('Book')
+
+    review = models.CharField(max_length=200, blank=True)
+    score = models.FloatField(default=0.0)
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.review
+
+
+class Image(models.Model):
+    book = models.ForeignKey('Book')
+
+    image = models.ImageField(
+        upload_to=get_book_image_path, height_field=100,
+        width_field=100, blank=True)
+
+    def __str__(self):
+        return self.book.title()
+
+
+class Category(models.Model):
+    name = models.CharField(
+        choices=CATEGORY_CHOICES, max_length=30, default='accounting')
+
+    def __str__(self):
+        return self.name.title()
