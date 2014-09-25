de15a29a09db71884654970521a1ebe73813b1e2
Emmanuel Luciano <emma.luciano11@gmail.com>
2014-09-24 20:07:43 -0400

    added recommended section
    recommended section is working, data mining still needs work

--- bookshub/books/admin.py
+++ bookshub/books/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book, Review, Image, Category, Requested
+from .models import Book, Review, Image, Category, Requested, Viewed
 
 from reversion import VersionAdmin
 
@@ -20,8 +20,13 @@ class ImageAdmin(VersionAdmin, admin.ModelAdmin):
 class RequestedAdmin(VersionAdmin, admin.ModelAdmin):
     list_display = ('title', 'author', 'status', 'isbn_10', 'isbn_13')
 
+
+class ViewedAdmin(VersionAdmin, admin.ModelAdmin):
+    list_display = ('user', 'book')
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Review, ReviewAdmin)
 admin.site.register(Image, ImageAdmin)
 admin.site.register(Category)
 admin.site.register(Requested, RequestedAdmin)
+admin.site.register(Viewed, ViewedAdmin)
