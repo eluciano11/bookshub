1706b3fa149dd062f57a57ccd017caf16939e900
Emmanuel Luciano <emma.luciano11@gmail.com>
2014-10-14 22:38:40 -0400

    added lines commented by error

--- bookshub/contact/models.py
+++ bookshub/contact/models.py
@@ -1,13 +1,13 @@
-# from django.db import models
+from django.db import models
 
-# from ..utils.models import BaseModel
-# from .constants import CATEGORY_CHOICES, STATUS_CHOICES
-# from ..users.models import User
+from ..utils.models import BaseModel
+from .constants import CATEGORY_CHOICES, STATUS_CHOICES
+from ..users.models import User
 
 
-# class Message(BaseModel):
-#     user = models.ForeignKey(User)
-#     subject = models.CharField(max_length=25, blank=False)
-#     body = models.CharField(max_length=300, blank=False)
-#     category = models.CharField(choices=CATEGORY_CHOICES, max_length=1)
-#     status = models.CharField(choices=STATUS_CHOICES, max_length=10)
+class Message(BaseModel):
+    user = models.ForeignKey(User)
+    subject = models.CharField(max_length=25, blank=False)
+    body = models.CharField(max_length=300, blank=False)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=1)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10)
