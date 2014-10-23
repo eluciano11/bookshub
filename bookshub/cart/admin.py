ad77eaaf745055036175ca217fe0d35669729553
Emmanuel Luciano <emma.luciano11@gmail.com>
2014-10-23 15:13:07 -0400

    merge conflict

--- bookshub/cart/admin.py
+++ bookshub/cart/admin.py
@@ -6,10 +6,7 @@ from reversion import VersionAdmin
 
 
 class OrderItemAdmin(VersionAdmin):
-<<<<<<< HEAD
     list_display = ('offer', 'user', 'quantity', 'is_purchased')
-=======
     list_display = ('user', 'offer', 'quantity', 'is_purchased')
->>>>>>> FETCH_HEAD
 
 admin.site.register(OrderItem, OrderItemAdmin)
