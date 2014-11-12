28d769ac3ac883e87772035288f0ae939de21920
Jose Padilla <jpadilla1@users.noreply.github.com>
2014-11-12 12:34:01 -0400

    added metadata on offer serializers

--- bookshub/offers/serializers.py
+++ bookshub/offers/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 
 from .models import Offer, Image
-from ..books.serializers import BookSimpleSerializer
+
+
+# class BookSimpleSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = Book
+#         fields = ('id', 'title', 'author', 'publisher', 'score',
+#                   'category', 'isbn_10', 'isbn_13', 'edition')
 
 
 class OfferSerializer(serializers.ModelSerializer):
@@ -20,7 +27,18 @@ class OfferSerializer(serializers.ModelSerializer):
         metadata = {
             "owner_name": self.obj.owner.get_full_name(),
             "owner_username": self.obj.owner.username,
-            "book": BookSimpleSerializer(obj.book)
+            "book": {
+                "title": self.obj.book.title,
+                "author": self.obj.book.author,
+                "isbn_10": self.obj.book.isbn_10,
+                "isbn_13": self.obj.book.isbn_13,
+                "edition": self.obj.book.edition,
+                "publisher": self.obj.book.publisher,
+                "score": self.obj.book.score,
+                "tags": self.obj.get_tags_display(),
+                "category": self.obj.book.category.name,
+                "image": self.obj.book.image.url
+            }
         }
         return metadata
 
