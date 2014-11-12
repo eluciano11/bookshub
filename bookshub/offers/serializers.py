811f4e2f8353b3153d3823a4b1e910eabe64bec3
Jose Padilla <jpadilla1@users.noreply.github.com>
2014-11-12 12:41:03 -0400

    removes self from get_metadata

--- bookshub/offers/serializers.py
+++ bookshub/offers/serializers.py
@@ -25,19 +25,19 @@ class OfferSerializer(serializers.ModelSerializer):
 
     def get_metadata(self, obj):
         metadata = {
-            "owner_name": self.obj.owner.get_full_name(),
-            "owner_username": self.obj.owner.username,
+            "owner_name": obj.owner.get_full_name(),
+            "owner_username": obj.owner.username,
             "book": {
-                "title": self.obj.book.title,
-                "author": self.obj.book.author,
-                "isbn_10": self.obj.book.isbn_10,
-                "isbn_13": self.obj.book.isbn_13,
-                "edition": self.obj.book.edition,
-                "publisher": self.obj.book.publisher,
-                "score": self.obj.book.score,
-                "tags": self.obj.get_tags_display(),
-                "category": self.obj.book.category.name,
-                "image": self.obj.book.image.url
+                "title": obj.book.title,
+                "author": obj.book.author,
+                "isbn_10": obj.book.isbn_10,
+                "isbn_13": obj.book.isbn_13,
+                "edition": obj.book.edition,
+                "publisher": obj.book.publisher,
+                "score": obj.book.score,
+                "tags": obj.get_tags_display(),
+                "category": obj.book.category.name,
+                "image": obj.book.image.url
             }
         }
         return metadata
