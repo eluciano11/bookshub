e1b85935af40524d51b797d9b46dbc584df23436
Jose Padilla <jpadilla1@users.noreply.github.com>
2014-11-12 12:09:24 -0400

    added metadata for offers serializers

--- bookshub/offers/serializers.py
+++ bookshub/offers/serializers.py
@@ -1,19 +1,28 @@
 from rest_framework import serializers
 
 from .models import Offer, Image
+from books.serializers import BookSimpleSerializer
 
 
 class OfferSerializer(serializers.ModelSerializer):
-    owner = serializers.RelatedField()
+    metadata = serializers.SerializerMethodField('get_metadata')
 
     class Meta:
         model = Offer
-        fields = ('owner', 'book', 'price', 'condition',
-                  'quantity', 'start_date', 'description')
+        fields = ('id', 'owner', 'book', 'price', 'condition',
+                  'quantity', 'start_date', 'description', 'metadata')
 
     def save_object(self, obj, **kwargs):
         obj.owner = self.context['request'].user
         super(OfferSerializer, self).save_object(obj, **kwargs)
+
+    def get_metadata(self, obj):
+        metadata = {
+            "owner_name": self.obj.owner.get_full_name(),
+            "owner_username": self.obj.owner.username,
+            "book": BookSimpleSerializer(obj.book)
+        }
+        return metadata
 
 
 class OfferImageSerializer(serializers.ModelSerializer):
