e720c01fc8ce6b90bf3730a44d74efcae6e7abc3
Jose Padilla <jpadilla1@users.noreply.github.com>
2014-12-02 22:01:01 -0400

    made transactions work enough to fake it

--- bookshub/cart/views.py
+++ bookshub/cart/views.py
@@ -1,8 +1,14 @@
+from django.core.mail import EmailMessage
+
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import OrderItem
 from .serializers import OrderItemSerializer
 from .permissions import OrderItemPermission
+
+from ..utils.response import ErrorResponse
 
 
 class OrderItemViewSet(ModelViewSet):
@@ -11,4 +17,33 @@ class OrderItemViewSet(ModelViewSet):
     permission_classes = (OrderItemPermission, )
 
     def get_queryset(self):
-        return self.model.objects.filter(user=self.request.user)
+        return self.model.objects.filter(
+            user=self.request.user, is_purchased=False)
+
+
+class BuyItemsInCartView(APIView):
+
+    def post(self, request):
+        cart_items = OrderItem.objects.filter(
+            user=request.user, is_purchased=False)
+
+        if len(cart_items) > 0:
+            for item in cart_items:
+                item.is_purchased = True
+
+                item.save()
+
+                email = EmailMessage(to=[request.user.email], from_email=None)
+
+                email.template_name = "bought-item"
+                email.use_template_subject = True
+                email.use_template_from = True
+
+                email.global_merge_vars = {
+                    "BUYER_NAME": request.user.get_full_name(),
+                    "BUYER_EMAIL": request.user.email,
+                }
+
+                email.send(fail_silently=False)
+            return Response({"success": True})
+        return ErrorResponse({"message": "cart must not be empty to checkout."})
